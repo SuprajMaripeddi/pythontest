6e414a0eb8b931659edb35d4f00ec118a03962c8
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2021-07-03 00:38:05 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -3,3 +3,7 @@ import os
 # get emails and password from environment variables
 Toolname = os.environ.get("TOOL_NAME")
 print(Toolname)
+if(Toolname=="terraform"):
+  print("value is terraform")
+else:
+  print("secret is not reading correctly")
